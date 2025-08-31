b6248cf302c221b87b7ae6b2567b3bc9d3008ca3
Luke Fernando <isanjaya139@gmail.com>
2025-09-01 00:51:33 +0530

    mobile navbar created

--- storefront/src/components/nav/MobileNav.tsx
+++ storefront/src/components/nav/MobileNav.tsx
@@ -1,5 +1,7 @@
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import Link from "next/link";
 
 export default function MobileNav() {
   return (
@@ -9,9 +11,61 @@ export default function MobileNav() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Shemaya Aqua Mart</SheetTitle>
           <SheetDescription>
-            This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-2" className="py-2">
+                <Link href="#" className="text-base text-black/90 hover:text-black font-light">
+                  Home
+                </Link>
+              </AccordionItem>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>Categories</AccordionTrigger>
+                <AccordionContent>
+                  <div className="w-full box-border pl-3">
+                    <Accordion type="single" collapsible className="border-l pl-10 box-border">
+                      <AccordionItem value="item-1">
+                        <AccordionTrigger className="text-sm">Home</AccordionTrigger>
+                        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+                      </AccordionItem>
+                      <AccordionItem value="item-2">
+                        <Link href="#" className="text-sm text-black/90 hover:text-black font-light">
+                          Is it accessible?
+                        </Link>
+                      </AccordionItem>
+                    </Accordion>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-3">
+                <AccordionTrigger>Services</AccordionTrigger>
+                <AccordionContent>
+                  <div className="w-full box-border pl-3">
+                    <Accordion type="single" collapsible className="border-l pl-10 box-border">
+                      <AccordionItem value="item-2" className="py-3">
+                        <Link href="#" className="text-sm text-black/90 hover:text-black font-light">
+                          Labrotary Services
+                        </Link>
+                      </AccordionItem>
+                      <AccordionItem value="item-2">
+                        <Link href="#" className="text-sm text-black/90 hover:text-black font-light">
+                          Advisor Services
+                        </Link>
+                      </AccordionItem>
+                    </Accordion>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-4" className="py-2">
+                <Link href="#" className="text-base text-black/90 hover:text-black font-light">
+                  Blog
+                </Link>
+              </AccordionItem>
+              <AccordionItem value="item-5" className="py-2">
+                <Link href="#" className="text-base text-black/90 hover:text-black font-light">
+                  Gallery
+                </Link>
+              </AccordionItem>
+            </Accordion>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
