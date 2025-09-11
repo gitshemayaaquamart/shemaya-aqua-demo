0e9b0980d9d8ab561968cf1aaab0559a401984d1
Luke Fernando <isanjaya139@gmail.com>
2025-09-12 00:33:48 +0530

    minor changes to the quick blogs and our products section

--- storefront/src/components/blogs/blog-row.tsx
+++ storefront/src/components/blogs/blog-row.tsx
@@ -2,7 +2,7 @@ import BlogCard from "./blog-card";
 
 export default function BlogRow() {
   return (
-    <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-y-8 sm:gap-y-0 gap-x-5 lg:gap-x-12 xl:gap-x-18 2xl:gap-x-28 box-border px-10 sm:px-3 md:px-14 lg:px-0 xl:px-10">
+    <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-y-8 sm:gap-y-0 gap-x-5 lg:gap-x-12 xl:gap-x-18 2xl:gap-x-28 box-border px-2 sm:px-3 md:px-14 lg:px-0 xl:px-10">
       <BlogCard />
       <BlogCard />
       <BlogCard className="hidden xl:flex" />
--- storefront/src/components/products/product-row.tsx
+++ storefront/src/components/products/product-row.tsx
@@ -2,7 +2,7 @@ import ProductCard from "./product-card";
 
 export default function ProductRow() {
   return (
-    <div className="w-full h-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-1 sm:gap-2 md:gap-x-16 lg:gap-3 xl:gap-14 2xl:gap-22 box-border sm:px-3 md:px-14 lg:px-0 xl:px-10">
+    <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 sm:gap-2 md:gap-x-16 lg:gap-3 xl:gap-14 2xl:gap-22 box-border px-2 sm:px-3 md:px-14 lg:px-0 xl:px-10">
       <ProductCard />
       <ProductCard />
       <ProductCard />
